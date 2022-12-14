63e47a0a00d6378f09cdab886d7409a7a588bce2
Yahya <yahyabinzakariyya@gmail.com>
2022-12-14 18:00:50 +0500

    result div uploaded

--- php/main.php
+++ php/main.php
@@ -33,20 +33,21 @@ include "navbar.php";
 
         <!-- Card Sample-1 -->
         <table class="table">
-            <div class="shadow-sm row mx-1 my-3">
-                <div class="col-lg-2 col-3 text-center bg-light border-end">
-                    <img class="rounded-circle col-10 border border-2" style="padding: 2px;" src="../images/ProfilePicture.jpg" alt="profile picture">
-                    <p class="bg-dark text-light m-0">Yahya</p>
-                </div>
-                <div class="col-lg-10 col-9 p-2 bg-light" >
+            <tr class="shadow-sm mx-1 my-3" style="height:120px;">
+                <td class="text-center bg-light border-end p-0 position-relative" style="min-width:120px;">
+                    <div class="py-1">
+                        <img class="col-9 rounded-circle border border-2 border-dark" style="padding: 2px;" src="../images/ProfilePicture.jpg" alt="profile picture">
+                    </div>
+                    <p class="bg-dark text-light m-0 position-absolute col bottom-0 end-0 start-0">Yahya</p>
+                </td>
+                <td class="p-2 bg-light" style="width:100%;">
                     <h5>Normalization</h5>
                     <sup>Uploaded: 12-11-2022</sup>
-                    <div class="row">
-                        <p class="text-nowrap col-8 m-0" style="overflow: hidden; text-overflow: ellipsis;">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem deleniti eos molestiae reiciendis dolore, rerum quasi eligendi dignissimos a accusamus ut dolores aut nulla quaerat amet cupiditate quo eveniet asperiores possimus in laborum id optio officiis. Ipsa dolores est excepturi!</p>
-                        <a class="col-4 link-secondary" href="">view</a>
-                    </div>
-                </div>
-            </div>
+                    <div style="white-space: nowrap; width: 200px; overflow: hidden;
+                    text-overflow: ellipsis;">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem deleniti eos molestiae reiciendis dolore, rerum quasi eligendi dignissimos a accusamus ut dolores aut nulla quaerat amet cupiditate quo eveniet asperiores possimus in laborum id optio officiis. Ipsa dolores est excepturi!</div>
+                    <a class="link-secondary" href="";">view</a>
+                </td>
+            </tr>
         </table>
 </section>
 
